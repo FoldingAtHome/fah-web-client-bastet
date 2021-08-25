7e8d0f6a4dc26a31ee337f68445f54a5a436f6bd
Shreya Shah <shreyashah690@gmail.com>
2021-08-25 21:14:53 +0530

    Basic Vue 3 App for FAH Frontend

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/fahlogo.png" />
+  <h2>Folding@Home Frontend</h2>
+  <AppPage msg="This page is under construction!" />
+</template>
+
+<script>
+import AppPage from './components/AppPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppPage
+  }
+}
+</script>
---
+++ src/components/AppPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'AppPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
