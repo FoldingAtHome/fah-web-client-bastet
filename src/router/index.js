b6d92baee9ca10dc406da75e533f4fb2f48bcae7
Shreya Shah <shreyashah690@gmail.com>
2021-11-20 18:46:03 +0530

    Remove feature3 from Navbar.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Units from '../components/Units.vue'
-import UnderConstruction from '../components/UnderConstruction.vue'
-import Details from '../components/Details.vue'
 
 const routes = [
 	{
@@ -18,11 +16,6 @@ const routes = [
 		path: '/workunit',
 		name: 'WorkUnit',
 		component: () => import(/* webpackChunkName: "workunit" */ '../components/WorkUnit.vue')
-	},
-	{
-		path: '/todo',
-		name: 'Todo',
-		component: UnderConstruction
 	}
 ]
 
