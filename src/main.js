30e1e623d8cc6a0b6422ab5a29cd8e9305613ff9
Shreya Shah <shreyashah690@gmail.com>
2021-11-30 19:47:10 +0530

    Added user information on main page.

--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ import App from './App.vue'
 import './index.styl'
 import 'bootstrap'
 import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { dom } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).use(router).mount('#app')
+library.add(fas);
+dom.watch();
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(router).mount('#app');
--- src/views/Units.vue
+++ src/views/Units.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 .view
+  UserCard(:key="config.team")
   h2 Work Units
-    button.btn.pauseBtn(type="button" :class="[config.paused ? 'btn-success' : 'btn-warning']" @click="setPause(!config.paused)")
-      | {{ config.paused ? "Start" : "Pause" }}
+    button.btn.pauseBtn(type="button" :class="[config.paused ? 'btn-success' : 'btn-warning']" 
+    @click="setPause(!config.paused)") {{ config.paused ? "Start" : "Pause" }}
   table.table
     thead
       tr
@@ -23,54 +24,52 @@
           td
             .progress
               .progress-bar.progress-bar-striped(role="progressbar",
-                :class="[unit.paused ? 'bg-secondary' : 'progress-bar-animated bg-success']",
+                :class="[unit['pause-reason'] ? 'bg-secondary' : 'progress-bar-animated bg-success']",
                 :style="{ width: unit.progress * 100 + '%' }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100")
                 | {{ (unit.progress * 100).toFixed() | 0 }}%
 </template>
 
 <script>
 import useWebSocket from "../composables/useWebSocket";
+import UserCard from "../components/UserCard.vue";
 
 export default {
-  name: 'Units',
+  name: "Units",
+  components: { UserCard },
   setup() {
     const { units, config, send } = useWebSocket;
-
     const status = {
       "DOWNLOAD": "Downloading workunit.",
       "CORE": "Downloading core.",
       "RUN": "Running.",
       "UPLOAD": "Uploading",
       "CLEAN": "Finished. Cleaning workunit."
-    }
+    };
 
     const getResources = (cpus, gpus) => {
       let msg = "";
       if(cpus > 1) msg += ("cpu:" + cpus + " ");
       if(gpus.length) {
-        msg += "gpu:"
-        let gpuList = gpus.map(gpu => gpu.split(' ')[0])
-        msg += gpuList.join(', ');
+        msg += "gpu:";
+        let gpuList = gpus.map(gpu => gpu.split(" ")[0]);
+        msg += gpuList.join(", ");
       }
       return msg;
-    }
+    };
 
     const getStatus = (pauseReason, state) => {
-      if(pauseReason && pauseReason != "") return pauseReason
-      else return status[state]
-    }
+      if(pauseReason && pauseReason != "") return pauseReason;
+      return status[state];
+    };
 
-    const setPause = (state) => { send({ cmd: state ? "pause" : "unpause" }) };
-
+    const setPause = (state) => { send({ cmd: state ? "pause" : "unpause" }); };
     const unitPRCG = (index) => {
       const unitData = units.value[index];
       if(unitData && unitData.assignment && unitData.wu)
-        return `${unitData.assignment.project} (${unitData.wu.run}, ${unitData.wu.clone}, ${unitData.wu.gen})`
-      else
-        return "Will be assigned shortly."
-    }
-
-    return { units, config, unitPRCG, getStatus, getResources, setPause }
+        return `${unitData.assignment.project} (${unitData.wu.run}, ${unitData.wu.clone}, ${unitData.wu.gen})`;
+      return "Will be assigned shortly.";
+    };
+    return { units, config, getResources, getStatus, setPause, unitPRCG };
   }
 }
 </script>
