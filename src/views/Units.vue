7a87a150ecb678f6f01a3c65c3dfe2f96899a60c
Shreya Shah <shreyashah690@gmail.com>
2021-12-27 19:43:59 +0530

    Show ETA and PPD.

--- src/views/Units.vue
+++ src/views/Units.vue
@@ -1,19 +1,21 @@
 <template lang="pug">
 .view
-  UserCard(:key="config.team")
+  UserCard(:key="config.team" :ppd="ppd")
   h2 Work Units
-    button.btn.pauseBtn(type="button" :class="[config.paused ? 'btn-success' : 'btn-warning']" 
+    button.btn.pauseBtn(type="button" :class="[config.paused ? 'btn-success' : 'btn-warning']"
     @click="setPause(!config.paused)") {{ config.paused ? "Start" : "Pause" }}
   table.table
     thead
       tr
         th(width="20%")
           | Project (Run, Clone, Gen)
-        th(width="25%")
-          | Resources
-        th(width="25%")
-          | Status
         th(width="30%")
+          | Resources
+        th(width="15%")
+          | Status
+        th(width="15%")
+          | ETA
+        th(width="20%")
           | Progress
     tbody
       template(v-for="(unit, index) in units" :key="index")
@@ -21,23 +23,27 @@
           td {{ unitPRCG(index) }}
           td {{ getResources(unit.cpus, unit.gpus) }}
           td {{ getStatus(unit["pause-reason"], unit.state) }}
+          td {{ unit.eta }}
           td
             .progress
               .progress-bar.progress-bar-striped(role="progressbar",
                 :class="[unit['pause-reason'] ? 'bg-secondary' : 'progress-bar-animated bg-success']",
-                :style="{ width: unit.progress * 100 + '%' }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100")
-                | {{ (unit.progress * 100).toFixed() | 0 }}%
+                :style="{ width: getProgress(unit['progress']) + '%'}" aria-valuemin="0" aria-valuemax="100")
+              span {{ getProgress(unit['progress'], 2) }}%
+
 </template>
 
 <script>
 import useWebSocket from "../composables/useWebSocket";
 import UserCard from "../components/UserCard.vue";
+import { ref, watch } from 'vue';
 
 export default {
   name: "Units",
   components: { UserCard },
   setup() {
     const { units, config, send } = useWebSocket;
+    const ppd = ref(0);
     const status = {
       "DOWNLOAD": "Downloading workunit.",
       "CORE": "Downloading core.",
@@ -57,6 +63,18 @@ export default {
       return msg;
     };
 
+    const getProgress = (progress, precision = 0) => {
+      if(isNaN(progress)) return 0;
+      return (progress * 100.0).toFixed(precision);
+    }
+
+    watch([() => units], () => {
+      ppd.value = 0;
+      for(let unit of units.value)
+        if(unit && !unit.hasOwnProperty("pause-reason") && !isNaN(unit['ppd']))
+          ppd.value += parseInt(unit['ppd']);
+    }, { deep: true });
+
     const getStatus = (pauseReason, state) => {
       if(pauseReason && pauseReason != "") return pauseReason;
       return status[state];
@@ -69,15 +87,28 @@ export default {
         return `${unitData.assignment.project} (${unitData.wu.run}, ${unitData.wu.clone}, ${unitData.wu.gen})`;
       return "Will be assigned shortly.";
     };
-    return { units, config, getResources, getStatus, setPause, unitPRCG };
+
+    return { units, config, ppd, getResources, getProgress, getStatus, setPause, unitPRCG };
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .pauseBtn
-  margin: 10px
+  margin 10px
 
 tr.disabled
   background-color #d3d3d3
+
+.progress
+  position relative
+  background-color #aab2ba
+
+.progress > span
+  position absolute
+  left 0
+  width 100%
+  text-align center
+  z-index 2
+  color white
 </style>
