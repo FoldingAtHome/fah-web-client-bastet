4207c18e50ca01fb269fb915c822090c99096b7f
Joseph Coffland <joseph@cauldrondevelopment.com>
2023-09-23 15:46:41 +0300

    Fixed vue.js custom tags warning

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: tag => ['tt', 'center'].includes(tag)
+      }
+    }
+  })]
 })
